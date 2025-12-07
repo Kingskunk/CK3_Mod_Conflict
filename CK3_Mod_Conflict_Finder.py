1dff2a5b8cd755cf6ec31032019ed5b137e69bf5
Arnab Biswas <arnabbiswas@hotmail.com>
2025-12-07 15:50:35 +0530

    Enhance CK3 Mod Conflict Finder functionality
    Updated the script to include additional features such as getting the documents path from Windows, handling exclusions from a resolved conflicts file, and improved file handling for mod conflicts.

--- CK3_Mod_Conflict_Finder.py
+++ CK3_Mod_Conflict_Finder.py
@@ -1,8 +1,12 @@
 
-import os	#OS file operations
-import json		#Mod load order
+import os	# OS file operations
+import json		# for Mod load order
+import ctypes # for Documents path
+import ctypes.wintypes # for Documents path
 
-write_2_file = False
+write_2_file:bool = True
+file_extensions:tuple[str, ...] = (".txt", ".gui", ".asset", ".py")
+
 print("\
 Launch the game launcher once before running this script!\n\
 Remove the mods from load order if the files have been deleted!\n\
@@ -12,43 +16,57 @@ Will cause issues if mod with exact same name are present!\n\
 ## --------------------------------------
 ## Setup Paths
 ## --------------------------------------
+this_file_path : str = os.path.dirname(os.path.abspath(__file__)).replace('\\','/')
+print("This file path =\n" + this_file_path)
+
 game_file_path : str = input("Game Path = ")
 if game_file_path == "":
-  game_file_path = "C:/Program Files (x86)/Steam/steamapps/common/Crusader Kings III/game"
+  game_file_path = "G:/SteamLibrary/steamapps/common/Crusader Kings III/game"
 game_file_path = game_file_path.replace('\\','/')
 print(game_file_path)
 
 steam_dir_path : str = input("Steam Mod Path = ")
 if steam_dir_path == "":
-  steam_dir_path = "C:/Program Files (x86)/Steam/steamapps/workshop/content/1158310"
+  steam_dir_path = "G:/SteamLibrary/steamapps/workshop/content/1158310"
+  
 steam_dir_path = steam_dir_path.replace('\\','/')
 print(steam_dir_path)
 
-docum_ck3_path : str = input("Documents Path = ")
-if docum_ck3_path == "":
-  docum_ck3_path = "B:/Documents/Paradox Interactive/Crusader Kings III"
-docum_ck3_path = docum_ck3_path.replace('\\','/')
-print(docum_ck3_path)
+# Get Documents Path from Windows
+buffer = ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH) #create buffer for path
+ctypes.windll.shell32.SHGetFolderPathW(None, 0x0005 , None, None, buffer) # get path to buffer (CSIDL value = 0x0005)
+documents_ck3_path :str = buffer.value.replace('\\','/') + '/Paradox Interactive/Crusader Kings III'
+
+print(documents_ck3_path)
 
 def open_file(path:str) -> any:
   with open(path, "r") as f:
     if(path.endswith(".json")):
-      var = json.load(f)
+      var = json.load(f)  # .json file as dictionary
     else:
-      var = f.readlines()
-  f.close
+      var = f.readlines() # Other file as list of lines
+  f.close()
   return var
+
+## --------------------------------------
+## Add any additional exclusions from resolved_conflicts file
+try:
+  filename_exclusions:list[str] = open_file(f'{this_file_path+'/resolved_conflicts.txt'}')
+except FileNotFoundError:
+  filename_exclusions:list[str] = []
+filename_exclusions = [exclusion.strip('\n') for exclusion in filename_exclusions]
+print("Exclusions:", filename_exclusions)
 
 ## --------------------------------------
 ## Get Load Order and create Dictionary out of it to get names for conflicts
 ## --------------------------------------
 
-load_order : dict[str] = open_file(f'{docum_ck3_path+'/dlc_load.json'}')["enabled_mods"]
+load_order : dict[str, str] = open_file(f'{documents_ck3_path+'/dlc_load.json'}')["enabled_mods"]
 
-mod_name_dict:dict[str, str]={}  #mod_name_dict.keys() = .mod file should be unique, mod_name_dict.values() = Name
+mod_name_dict:dict[str, str] = {}  #mod_name_dict.keys() = .mod file should be unique, mod_name_dict.values() = Name
 
 for local_mod_file in load_order:
-  mod_descriptor = open_file((f'{docum_ck3_path+'/'+local_mod_file}'))
+  mod_descriptor = open_file((f'{documents_ck3_path+'/'+local_mod_file}'))
 
   for line in mod_descriptor:
     l = line.strip("\"\n")
@@ -77,32 +95,34 @@ for i in mod_name_dict_flipped.items():
 mod_name_dict_flipped.clear()
 
 if write_2_file:
-  with open(f'{docum_ck3_path+'/mod_name_dict.txt'}', 'w') as f:
+  with open(f'{this_file_path+'/mod_name_dict.txt'}', 'w') as f:
     f.write((json.dumps(mod_name_dict,indent=0,ensure_ascii=False)))
 
 ## --------------------------------------
 ## Search all files from Setup Paths with the following extensions
 ## --------------------------------------
 
-def make_files_list(path : str) -> list[str]:
-  file_list:dict[str, str] = {}
-  for dirpath, subdirs, files in os.walk(path):
+def make_files_list(path : str) -> dict[str,str]:
+  file_list:dict[str,str] = {}
+  for dirpath, _, files in os.walk(path):
     for file in files:
-      if file.lower().endswith((".txt", ".gui", ".asset", ".anim")):
-        file_list[f'{(dirpath+'/'+file).replace('\\','/')}'] = file
+      if file.lower().endswith(file_extensions) : # only specified extensions
+        if any(exclusions in file.lower() for exclusions in filename_exclusions) == False: # exclude specified filenames
+          file_list[f'{(dirpath+'/'+file).replace('\\','/')}'] = file
   return(file_list)
 
 ## --------------------------------------
 ## add all files to dict, flip dict to extract duplicates
 ## --------------------------------------
 all_files:dict[str, str] = {}
-all_files.update(make_files_list(f'{docum_ck3_path+'/mod'}'))
+all_files.update(make_files_list(f'{documents_ck3_path+'/mod'}'))
 all_files.update(make_files_list(game_file_path))
 all_files.update(make_files_list(steam_dir_path))
 
 flipped_files = flip_this_dict(all_files)
 all_files.clear()
 
+## Remove dupes
 mods_conflicts:dict = {}
 for key, value in flipped_files.items():
   if len(value)>1:
@@ -110,7 +130,7 @@ for key, value in flipped_files.items():
 flipped_files.clear()
 
 if write_2_file:
-  with open(f'{docum_ck3_path+'/mods_conflicts.txt'}', 'w') as f:
+  with open(f'{this_file_path+'/mods_conflicts.txt'}', 'w') as f:
     f.write((json.dumps(mods_conflicts,indent=0,ensure_ascii=False)))
 
 ## --------------------------------------
@@ -127,11 +147,21 @@ for file_name,locations in mods_conflicts.items():
         if file_name in named_conflicts:
           named_conflicts[file_name].append((order,mod_name,location)) #Add to previous entry
         else:
-          named_conflicts[file_name] = [(order,mod_name,location)]
+          named_conflicts[file_name] = [(order,mod_name,location)] #Create the entry if it doesn't exist
 
 for file_name,_ in named_conflicts.items():
   named_conflicts[file_name] = sorted(named_conflicts[file_name], key = lambda val: (val[0],val[2]))
 
-with open(f'{docum_ck3_path+'/named_conflicts.txt'}', 'w') as f:
-  f.write((json.dumps(named_conflicts,indent=2,ensure_ascii=False)))
+
+## Extract only real conflicts (more than 1 mod involved)
+final_conflicts:dict = {}
+for key, value in named_conflicts.items():
+  if len(value)>1 : # and key not in resolved_conflicts:
+    final_conflicts[key] = value
+named_conflicts.clear()
+
+
 #print(named_conflicts)
+
+with open(f'{this_file_path+'/final_conflicts.txt'}', 'w') as f:
+  f.write((json.dumps(final_conflicts,indent=2,ensure_ascii=False)))
